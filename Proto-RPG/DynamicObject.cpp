7b21dbf8896a21b7bf2c4d7aae29db6c8c2add5f
Alessandro Bria <a.bria@unicas.it>
2024-11-05 10:15:51 +0100

    cleaning comments

--- Proto-RPG/DynamicObject.cpp
+++ Proto-RPG/DynamicObject.cpp
@@ -24,7 +24,6 @@ DynamicObject::DynamicObject(Scene* scene, const RectF& rect, Sprite* sprite, in
 	_facingDir = Direction::DOWN;
 }
 
-// overrides MovableObject's move (+facing dir)
 void DynamicObject::move(Direction xDir, Direction yDir)
 {
 	if (xDir != Direction::NONE && xDir != _facingDir)
@@ -32,8 +31,6 @@ void DynamicObject::move(Direction xDir, Direction yDir)
 	if (yDir != Direction::NONE && yDir != _facingDir)
 		_facingDir = yDir;
 
-	//if(name().compare(0, 7, "Soldier") == 0)
-	//	printf("move %s %s\n", dir2str(xDir).c_str(), dir2str(yDir).c_str());
 	CollidableObject::move(xDir, yDir);
 }
 

3b92bac45f2a12938f37f1c53ea16fb310935ff6
Alessandro Bria <a.bria@unicas.it>
2024-11-25 10:50:31 +0100

    made compatible with Fonts singleton

--- Proto-RPG/LevelLoader.cpp
+++ Proto-RPG/LevelLoader.cpp
@@ -44,15 +44,6 @@ Scene* LevelLoader::load(const std::string& name)
 		
 		// Link house (indoor)
 		RenderableObject* linkHouse = new RenderableObject(world, RectF(-16, -14, 16, 14), spriteLoader->get("linkhouse"), 0);
-		new StaticObject(world, RectF(-16, -9.5f, 16, 0.1f), nullptr, 1);
-		new StaticObject(world, RectF(-14, -14, 0.1f, 14), nullptr, 1);
-		new StaticObject(world, RectF(-2, -14, 0.1f, 14), nullptr, 1);
-		new StaticObject(world, RectF(-16, -1.5f, 7, 0.1f), nullptr, 1);
-		new StaticObject(world, RectF(-7, -1.5f, 7, 0.1f), nullptr, 1);
-
-		// Outdoor static colliders
-		// houses, walls, ...
-		new StaticObject(world, RectF(137, 171, 6, 6), nullptr, 1);
 
 		// NPCs
 		new Soldier(world, PointF(130, 185), RectF(133, 178, 2, 3));
@@ -60,14 +51,6 @@ Scene* LevelLoader::load(const std::string& name)
 		// player
 		Link* player = new Link(world, PointF(140, 179));
 		world->setPlayer(player);
-
-		// outdoor-to-indoor teleports + transitions
-		new Trigger(world, RectF(139.5f, 176.5f, 1.0f, 0.5f), player, [player, world, linkHouse]()
-			{
-				player->setPos({-8, -7});
-				world->toggleCameraFollowsPlayer();
-				world->view()->setRect(linkHouse->rect());
-			});
 
 		return world;
 	}
